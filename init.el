895764b9ee2fbec7a9f19dbced295f436c6e9c98
Lucas Ward <Lucas Ward>
2013-05-06 22:14:04 -0500

    a couple of fixes to init.el

--- init.el
+++ init.el
@@ -81,6 +81,12 @@
 (require 'autopair)
 (autopair-global-mode t)
 
+(autoload 'markdown-mode "markdown-mode"
+   "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
 ;;=======================================
 ;; Add hooks
 
